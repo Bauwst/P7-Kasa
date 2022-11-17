528c9853ff7f4d1da56865fe40769e864e397fe5
Bauwst <bauwst@gmail.com>
2022-11-17 03:20:30 +0100

    Début page A propos

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import backgroundAbout from '../Images/BackgroundAbout.png'
 import '../styles/About.css'
 import Banner from '../components/Banner'
 import React , {useEffect} from 'react'
-
+import Collapse from '../components/Collapse'
 
 
 const About = () => {
