75cbc509de992ca6dd4edb0986ffa664fcd736e6
Bauwst <bauwst@gmail.com>
2022-12-02 03:49:34 +0100

    Responsive & CSS terminés sur HOME, Erreur404 et A propos

--- src/components/Logement.jsx
+++ src/components/Logement.jsx
@@ -10,10 +10,17 @@ const Logement = ({ title, id, location, tags, host, hostPhoto, rating}) => {
                     <hi className="title-logement">{title}</hi>
                     <p className="location-logement">{location}</p>
                     <ul className ="tags-logement">
-                        {/* {tags.map((tag) =>
-                        <li key ={`${id}+ " " +${tag}`}></li>
-                        )} */}
+                        {/* {tags.map((tags) =>
+                            <li key ={`${id}+ " " +${tags}`}></li>
+                        )}  */}
                     </ul>
+                    </div>
+                    <div className = "host-note">
+                        <div className = "host">
+                            <p>{host.name}
+                            </p>
+                            <img className ="host-photo" src={hostPhoto} alt={host}/>
+                        </div>
                     <Note evaluation={rating} />
                 </div>
             </div>
